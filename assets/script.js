4fe3eff0ad3e6c677feb9a209f184f6ad548e0cc
BreeNous <breeanna.nouskhajian@gmail.com>
2024-05-22 19:26:14 -0400

    set all the data for current weather properly

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,19 @@ function getLocation() {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
 
         const location = {
             lat: data[0].lat,
             lon: data[0].lon
         }
-        console.log(location);
-
-        getWeather(location.lat, location.lon)
+        return location;
+    })
+    .then(function(location) {
+        currentWeather(location.lat, location.lon);
+        return location;
+    })
+    .then(function(location) {
+        futureWeather(location.lat, location.lon);
     })
     .catch(function(error) {
         console.log('Error fetching location data:', error);
@@ -26,10 +30,10 @@ function getLocation() {
    
 }
 
-function getWeather(lat, lon) {
+function currentWeather(lat, lon) {
     const locLat = lat;
     const locLon = lon;
-    const reqWetUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${locLat}&lon=${locLon}&appid=${apiKey}`;
+    const reqWetUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${locLat}&lon=${locLon}&units=metric&appid=${apiKey}`;
 
     fetch(reqWetUrl)
     .then(function (response) {
@@ -38,13 +42,72 @@ function getWeather(lat, lon) {
     .then(function (data) {
       console.log(data);
 
-      const weatherData = {
-        cityId: data.city.id,
-        cityName: data.city.name,
-
-
+      const timestamp = data.dt;
+      const date = new Date(timestamp * 1000);
+    
+      const currentWeather = {
+        cityName: data.name,
+        date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`,
+        icon: data.weather[0].icon,
+        temp: `${Math.round((data.main.temp + 32))} °F`,
+        wind: `${data.wind.speed} mph`,
+        humidity: `${data.main.humidity} %`
       }
-      console.log(weatherData);
+      console.log(currentWeather);
+      return currentWeather
+    })
+};    
+
+function futureWeather(lat, lon) {
+    const locLat = lat;
+    const locLon = lon;
+    const reqWetUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${locLat}&lon=${locLon}&units=metric&appid=${apiKey}`;
+
+    fetch(reqWetUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      const dayOneWeather = {
+        date: data.list[0].dt_txt,
+        icon: '',
+        temp: '',
+        wind: '',
+        humidity: ''
+      }
+      const dayTwoWeather = {
+        date: data.list[0].dt_txt,
+        icon: '',
+        temp: '',
+        wind: '',
+        humidity: ''
+      }
+      const dayThreeWeather = {
+        date: data.list[0].dt_txt,
+        icon: '',
+        temp: '',
+        wind: '',
+        humidity: ''
+      }
+      const dayFourWeather = {
+        date: data.list[0].dt_txt,
+        icon: '',
+        temp: '',
+        wind: '',
+        humidity: ''
+      }
+      const dayFiveWeather = {
+        date: data.list[0].dt_txt,
+        icon: '',
+        temp: '',
+        wind: '',
+        humidity: ''
+      }
+      console.log(currentWeather);
+      let fiveDaysArr = [dayOneWeather, dayTwoWeather, dayThreeWeather, dayFourWeather, dayFiveWeather];
+      return fiveDaysArr;
     })  
 }
 
