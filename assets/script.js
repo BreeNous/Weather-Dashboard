98a1d911f66d340837a61ae40498aba7ee44baed
BreeNous <breeanna.nouskhajian@gmail.com>
2024-05-23 19:30:27 -0400

    fixed the temp issue that was displaying wrong temp

--- assets/script.js
+++ assets/script.js
@@ -32,7 +32,7 @@ function getLocation(cityName) {
 function currentWeather(lat, lon) {
     const locLat = lat;
     const locLon = lon;
-    const reqWetUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${locLat}&lon=${locLon}&units=metric&appid=${apiKey}`;
+    const reqWetUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${locLat}&lon=${locLon}&units=imperial&appid=${apiKey}`;
 
     fetch(reqWetUrl)
     .then(function (response) {
@@ -44,6 +44,7 @@ function currentWeather(lat, lon) {
       const date = new Date(timestamp * 1000);
 
       const todaysFc = document.getElementById('nowWeather');
+      todaysFc.innerHTML = '';
       const todayHeader = document.createElement('div')
       todaysFc.appendChild(todayHeader);
 
@@ -56,7 +57,7 @@ function currentWeather(lat, lon) {
       todaysFc.appendChild(todayBody);
 
       const todayTemp = document.createElement('h3');
-      todayTemp.textContent = `Temp: ${Math.round((data.main.temp + 32))} °F`;
+      todayTemp.textContent = `Temp: ${Math.round(data.main.temp)} °F`;
       todayBody.appendChild(todayTemp);
 
       const todayWind = document.createElement('h3');
@@ -73,7 +74,7 @@ function currentWeather(lat, lon) {
 function futureWeather(lat, lon) {
     const locLat = lat;
     const locLon = lon;
-    const reqWetUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${locLat}&lon=${locLon}&units=metric&appid=${apiKey}`;
+    const reqWetUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${locLat}&lon=${locLon}&units=imperial&appid=${apiKey}`;
 
     fetch(reqWetUrl)
     .then(function (response) {
@@ -81,6 +82,10 @@ function futureWeather(lat, lon) {
     })
     .then(function (data) {
         console.log(data)
+
+      const fiveDays = document.getElementById('nowWeather2');
+      fiveDays.innerHTML = '';
+
       const dayOne = new Date(data.list[4].dt * 1000);
       const dayTwo = new Date(data.list[12].dt * 1000);
       const dayThree = new Date(data.list[20].dt * 1000); 
@@ -90,35 +95,35 @@ function futureWeather(lat, lon) {
       const dayOneWeather = {
         date: `${dayOne.getMonth() + 1}/${dayOne.getDate()}/${dayOne.getFullYear()}`,
         icon: data.list[4].weather[0].icon,
-        temp: `${Math.round((data.list[4].main.temp + 32))} °F`,
+        temp: `${Math.round(data.list[4].main.temp)} °F`,
         wind: `${data.list[4].wind.speed} mph`,
         humidity: `${data.list[4].main.humidity} %`
       }
       const dayTwoWeather = {
         date: `${dayTwo.getMonth() + 1}/${dayTwo.getDate()}/${dayTwo.getFullYear()}`,
         icon: data.list[12].weather[0].icon,
-        temp: `${Math.round((data.list[12].main.temp + 32))} °F`,
+        temp: `${Math.round(data.list[12].main.temp)} °F`,
         wind: `${data.list[12].wind.speed} mph`,
         humidity: `${data.list[12].main.humidity} %`
       }
       const dayThreeWeather = {
         date: `${dayThree.getMonth() + 1}/${dayThree.getDate()}/${dayThree.getFullYear()}`,
         icon: data.list[20].weather[0].icon,
-        temp: `${Math.round((data.list[20].main.temp + 32))} °F`,
+        temp: `${Math.round(data.list[20].main.temp)} °F`,
         wind: `${data.list[20].wind.speed} mph`,
         humidity: `${data.list[20].main.humidity} %`
       }
       const dayFourWeather = {
         date: `${dayFour.getMonth() + 1}/${dayFour.getDate()}/${dayFour.getFullYear()}`,
         icon: data.list[28].weather[0].icon,
-        temp: `${Math.round((data.list[28].main.temp + 32))} °F`,
+        temp: `${Math.round(data.list[28].main.temp)} °F`,
         wind: `${data.list[28].wind.speed} mph`,
         humidity: `${data.list[28].main.humidity} %`
       }
       const dayFiveWeather = {
         date: `${dayFive.getMonth() + 1}/${dayFive.getDate()}/${dayFive.getFullYear()}`,
         icon: data.list[36].weather[0].icon,
-        temp: `${Math.round((data.list[36].main.temp + 32))} °F`,
+        temp: `${Math.round(data.list[36].main.temp)} °F`,
         wind: `${data.list[36].wind.speed} mph`,
         humidity: `${data.list[36].main.humidity} %`
       }
